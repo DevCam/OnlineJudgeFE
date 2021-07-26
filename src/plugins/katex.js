0daaf5cc0f0e2c2cee24c95c11fb89ccf63e678e
DevCam <josean.camarena@gmail.com>
2021-07-26 18:19:44 -0500

    fix katex delimiter precedence limitations

--- src/plugins/katex.js
+++ src/plugins/katex.js
@@ -10,9 +10,7 @@ const defaultOptions = {
   throwOnError: false,
   delimiters: [
     {left: '$$', right: '$$', display: true},
-    {left: '$', right: '$', display: false},
-    {left: '\\[', right: '\\]', display: true},
-    {left: '\\(', right: '\\)', display: false}
+    {left: '$', right: '$', display: false}
   ]
 }
 
